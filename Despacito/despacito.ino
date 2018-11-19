454ca9b4da047b71fb00190ad91bda3ac766b4cc
Michael Brabec <brabemi@UZLABINA.LAN>
2018-11-19 10:43:43 +0100

    Github, play Despacito

---
+++ Despacito/despacito.ino
@@ -0,0 +1,55 @@
+/*arduino, play despacito
+*/
+ 
+#include "pitches.h"
+
+//notes in the melody:
+int melody[] = {
+  NOTE_D5, NOTE_CS5, NOTE_B4, NOTE_FS4,
+  NOTE_FS4, NOTE_FS4, NOTE_FS4, NOTE_FS4, NOTE_B4, NOTE_B4, NOTE_B4, NOTE_B4, NOTE_B4, NOTE_A4, NOTE_B4, NOTE_G4,
+  NOTE_G4, NOTE_G4, NOTE_G4, NOTE_G4, NOTE_B4, NOTE_B4, NOTE_B4, NOTE_B4, NOTE_B4, NOTE_CS4, NOTE_D5, NOTE_A4,
+  NOTE_A4, NOTE_A4, NOTE_A4, NOTE_A4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_E4, NOTE_E4, NOTE_CS4,  
+  NOTE_D5, NOTE_CS5, NOTE_B4, NOTE_FS4,
+  NOTE_FS4, NOTE_FS4, NOTE_FS4, NOTE_FS4, NOTE_B4, NOTE_B4, NOTE_B4, NOTE_B4, NOTE_B4, NOTE_A4, NOTE_B4, NOTE_G4,
+  NOTE_G4, NOTE_G4, NOTE_G4, NOTE_G4, NOTE_B4, NOTE_B4, NOTE_B4, NOTE_B4, NOTE_B4, NOTE_CS4, NOTE_D5, NOTE_A4,
+  NOTE_A4, NOTE_A4, NOTE_A4, NOTE_A4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_E4, NOTE_E4, NOTE_CS4, 
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc
+int noteDurations[] = {
+  2, 2, 4, 4,
+  8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 4, 4,
+  8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 4, 4,
+  8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 4, 2,
+  2, 2, 4, 4,
+  8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 4, 4,
+  8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 4, 4,
+  8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 4, 2,
+  
+  
+};
+
+void setup(){
+  //iterate over the notes of the melody:
+  for (int thisNote = 0; thisNote < sizeof(melody) / sizeof(melody[0]); thisNote++) {
+
+    /*
+      to calculate the note duration, take one second divided by the note type.
+      e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+     */
+    int noteDuration = 1000/noteDurations[thisNote];
+    tone(9, melody[thisNote],noteDuration);
+
+    /*
+      to distinguish the notes, set a minimum time between them.
+      the note's duration + 30% seems to work well:
+     */
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    
+    noTone(9); //stop the tone playing:
+  }
+  digitalWrite(8,LOW);
+}
+
+void loop(){ /*press reset to replay. */ }
